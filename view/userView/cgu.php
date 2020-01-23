1c2b783ebf0ca7b21bdebae9abbb5b7b7ef97c28
HadrienFourniere <55877304+HadrienFourniere@users.noreply.github.com>
2020-01-23 13:31:56 +0100

    test

---
+++ view/userView/cgu.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="cgu.css">
+</head>
+
+<body>
+
+
+<p>
+
+    Conditions générales d’utilisation du site PsycHealth </br>
+    </br>
+    Préambule </br>
+    Les présentes conditions générales d’utilisation sont conclues entre :</br>
+    -	Le gérant du site internet, ci-après désigné « l’Editeur »,</br>
+    -	Toute personne souhaitant accéder au site et à ses services, ci-après appelé « l’Utilisateur ».</br>
+    </br>
+    Article 1 – Principes</br>
+    Les présentes conditions générales d’utilisation ont pour objet l’encadrement juridique de l’utilisation du site PsycHealth et de ses services.</br>
+    Le site Internet PsycHealth est un service de :</br>
+    -	Gestion de tests psychotechniques pour des ouvriers de chantier </br>
+    -	Située au 10 rue de Vanves, 92170 Issy-les-Moulineaux, France.</br>
+    -	Courriel : moduloheart@gmail.com</br>
+    -	Numéro de téléphone : 09 45 65 45 68</br>
+    Les conditions générales d’utilisation doivent être acceptées par tout Utilisateur, et son accès au site vaut acception de ces conditions.</br>
+    </br>
+    Article 2 – Evolution et durée des CGU</br>
+    Les présentes conditions générales d’utilisation sont conclues pour une durée indéterminée. Le contrat produit ses effets à l’égard de l’Utilisateur à </br>
+    Le site PsycHealth se réserve le droit de modifier les clauses de ces conditions générales d’utilisation à tout moment et sans justification.</br>
+    </br>
+    Article 3 – Accès au site </br>
+    Tout Utilisateur ayant accès à internet peut accéder gratuitement et depuis n’importe où au site PsycHealth. Les frais supportés par l’Utilisateur pour y</br> accéder ne sont pas à la charge de l’Editeur.</br>
+    Le site et ses différents services peuvent être interrompus ou suspendus par l’Editeur, notamment à l’occasion d’une maintenance, sans obligation de</br> préavis ou de justification.</br>
+</p>
+
+</body>
+</html>
